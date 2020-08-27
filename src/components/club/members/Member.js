962bd129cd13d3c0851940a461fc8f606af1562e
Mandel <a.mandel06@gmail.com>
2020-08-27 02:12:33 +0200

    text members and product

--- src/components/club/members/Member.js
+++ src/components/club/members/Member.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
+import Loader from '../../utils/loader';
 // import './members.css';
 
 
@@ -19,49 +20,59 @@ const Members = (props) => {
 
   return(
     <div className="containerMemberClub">
+       {(props.members) ? (
+          <>
 
-        <nav className="navClub">
+         <nav className="navClub">
 
-               <Link to="/club/gym">
-                  <div className="menu menuMin">
-                     <img src='/gym.jpg' alt="gymnase" />
-                     <h4>GYM</h4>
-                  </div>
-               </Link> 
-
-
-
-               <Link to="/club/intro">
-                  <div className="menu menuMinClub menuMin">
-                     <img src='/club.png' alt="club" />
-                     <h4>Présentation du Club</h4>
-                  </div>   
-               </Link> 
+            <Link to="/club/gym">
+               <div className="menu menuMin">
+                  <img src='/gym.jpg' alt="gymnase" />
+                  <h4>GYM</h4>
+               </div>
+            </Link> 
 
 
 
-               <Link to="/club/members">
-                  <div className="menu menuMin">
-                     <img src='/staff.jpg' alt="staff"/>
-                  <h4>Membres</h4>
-                  </div>
-               </Link>
+            <Link to="/club/intro">
+               <div className="menu menuMinClub menuMin">
+                  <img src='/club.png' alt="club" />
+
+
+            <h4>Présentation du Club</h4>
+               </div>   
+            </Link> 
+
+
+
+            <Link to="/club/members">
+               <div className="menu menuMin">
+                  <img src='/staff.jpg' alt="staff"/>
+               <h4>Membres</h4>
+               </div>
+            </Link>
          </nav>
 
 
-      <h2> Membres</h2>
+         <h2> Membres</h2>
 
-      {
-        filteredMembers.map(member => (
-          <div className="divEachMember" key={Math.random()}>
-             <img src={member.picture} alt={member.poste}/>
- 
-             <div className="divEachMemberText">
-                <p>{member.position}</p><br/>
-                <p>{member.firstName} {member.lastName}</p><br/>
-             </div>
-          </div>
-       ))
+         {
+            filteredMembers.map(member => (
+            <div className="divEachMember" key={Math.random()}>
+            <img src={member.picture} alt={member.poste}/>
+
+            <div className="divEachMemberText">
+            <p>{member.position}</p><br/>
+            <p>{member.firstName} {member.lastName}</p><br/>
+            </div>
+            </div>
+            ))          
+         }  
+         </> 
+
+       ) : (<Loader />)
+
+        
       }
     </div>
   )
