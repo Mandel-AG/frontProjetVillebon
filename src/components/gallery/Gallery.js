600d72f005b006644d09ed28ca486feb80b51c5d
Mandel <a.mandel06@gmail.com>
2022-02-02 20:19:43 +0100

    update gallery

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 import './gallery.css';
 
 class Gallery extends Component{
@@ -6,30 +7,41 @@ class Gallery extends Component{
       super(props)
 
       this.state= {
-         filteredMedias : []
+         filteredMedias : [...this.props.medias]
       }
    }
 
 
-      filterMedias = (type) => {
-         const medias = this.props.medias;
-         const newMedias = [ ...medias];
-         const filtered = newMedias.filter( media => media.mediaType === type)
-         this.setState({
-            filteredMedias : filtered
-         })
-      }
+   componentDidMount(){
+      const medias = this.props.medias;
+      const newMedias = [ ...medias];
+      const filtered = newMedias.filter( media => media.mediaType === 'game')
+      this.setState({
+         filteredMedias : filtered
+      })
+      console.log('filtered media', this.state.filteredMedias)
+   }
+   
+   filterMedias = (type) => {
+      const medias = this.props.medias;
+      const newMedias = [ ...medias];
+      const filtered = newMedias.filter( media => media.mediaType === type)
+      this.setState({
+         filteredMedias : filtered
+      })
+   }
+   
       
-
-   render(){
-      const mediaList = this.state.filteredMedias.map(media => (
-         <div className='eachMedia' key={media.id}>
+      render(){
+         const mediaList = this.state.filteredMedias.map(media => (
+            <div className='eachMedia' key={media.id}>
             <img src={ media.picture } alt={ media.name } />
             <p>{ media.name }</p>
             <p>{ media.description }</p>
          </div>
       ))
-            
+      console.log('mount', this.state.filteredMedias)
+      
       
       return(
          <div className="containerGallery">
@@ -41,6 +53,7 @@ class Gallery extends Component{
             <div className='divGallery'>
                { mediaList }
             </div>            
+         
          </div>
 
       )
